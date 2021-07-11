824420ff5e9bfd12694ccea1942101464a742e87
Andrew Moraes <andrewmoraesds@gmail.com>
2021-07-11 10:22:37 -0300

    teste pipeline com aprovacao manual

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Som, som... testando'));
+app.get('/', (req, res) => res.send('Mais um teste'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
